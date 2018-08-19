9d93ed68a3319d30d81c25c836f71275f706533e
李璐慧 <liluhui@192.168.0.142>
2018-08-19 20:45:41 +0800

    init [empty] :sweat_drops:

--- bin/nightwatch.conf.js
+++ bin/nightwatch.conf.js
@@ -8,6 +8,8 @@ module.exports = {
   "src_folders": [
     "bin/tests/gettable.js"// Where you are storing your Nightwatch e2e tests
   ],
+  "custom_assertions_path": ["bin/custom_assertions"],
+  "custom_commands_path": ["bin/custom_commands"],
   "output_folder": "reports", // reports (test outcome) output by nightwatch
   "selenium": {
     "start_process": true, // tells nightwatch to start/stop the selenium process
--- bin/tests/empty.js
+++ bin/tests/empty.js
@@ -1,8 +1,12 @@
 const path = require('path')
+const fs = require('fs')
+const expect = require('chai').expect
 const app = require(path.resolve(__dirname, '../tmp/config.json'))
+const Util = require(path.resolve(__dirname, '../util'))
+let data
 let doTest = {
-	'空值检查: 是否有哪几列为空': checkCols,
-	'空值检查: 是否有为空的单元格': checkCel
+	'空值检查:是否有数据': checkTbody,
+	'空值检查：为空单元格': checkCel
 }
 
 // TODO if 如果没有一行内容， 有无内容提示
@@ -19,12 +23,62 @@ let emptyStr = ['--', '-', '']
 */
 
 function initData(data) {
+	data = data
 }
-function checkCols() {
-	console.log(111)
+
+function checkTbody (browser) {
+	browser.execute(function(tbody_dom) {
+		let $tds = document.querySelector(tbody_dom + ' td')
+		// Case: 没有数据
+		// 没有td
+		// tbody有其他节点并且有内容
+		return !!$tds
+	}, [app['table_dom']['tbody_dom']], function(rst) {
+		if (rst.value) {
+			browser.verify.equal(rst.value, true)
+		} else {
+			browser.expect.element(app['table_dom']['tbody_dom']).text.not.equal('')
+		}
+	});
 }
-function checkCel() {
-	console.log(222)
+
+function checkCel(browser) {
+	browser.execute( function(thead_dom, tbody_dom) {
+		let $ths = document.querySelectorAll(thead_dom + ' th,' + thead_dom + ' td')
+		let $trs = document.querySelectorAll(tbody_dom + ' tr')
+		let data = []
+		for (let th of $ths) {
+			data.push({name: th.innerText, data: []})
+		}
+
+		for (let j = 0; j < $ths.length; j++) {
+			for (let i = 0; i < $trs.length; i++) {
+				data[j].data.push($trs[i].querySelector('td:nth-child('+ (j+1) +')').innerText)
+			}
+		}
+		return data
+	}, [app['table_dom']['thead_dom'], app['table_dom']['tbody_dom']], function(rst) {
+		let colsEmpty = []
+		for (let c in rst.value) {
+			let cols = rst.value.data
+			let arr = []
+			for (let r in cols) {
+				if (isEmpty[cols[r]]) {
+					arr.push(r)
+				}
+			}
+			colsEmpty.push(arr)
+			browser.verify.equal(arr.length, 0)
+			if (!arr.length) {
+				Util.successLog('第' + (+c+1) + '列['+ rst.value[c].name +']数据都存在')
+			} else if (arr.length === rst.value[c].data.length) {
+				Util.errorLog('第' + (+c+1) + '列['+ rst.value[c].name +']数据全空')
+			} else {
+				Util.errorLog('第' + (+c+1) + '列['+ rst.value[c].name +']中以下几行数据为空 -> [' + arr.join(',') + ']')
+			}
+		}
+		// fs.writeFile('bin/tmp/t.json', JSON.stringify(rst.value))
+	})
 }
 function isEmpty(str) {
 	str = str + ''
