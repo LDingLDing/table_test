7aeed3943b823dadc4a9443b299c9203ccd42655
songce <songce@myhexin.com>
2018-08-22 14:00:38 +0800

    fix custom_assertions continue\

--- bin/custom_assertions/checkUnit.js
+++ bin/custom_assertions/checkUnit.js
@@ -4,15 +4,16 @@ exports.assertion = function (colDatas, colName, unit) {
   this.pass = function (data) {
     let checkFlag = [];
     for (let i in data.texts) {
+      i = Number(i)
       let text = data.texts[i]
       if (isNaN(parseFloat(text))) {
       }
       if (Math.abs(parseFloat(text)) > 10000) {
-        checkFlag.push('第'+(i+1)+'行缺少单位：万或亿；')
+        checkFlag.push('第'+(i+1)+'行缺少单位：万或亿；\n')
       }
       if (unit != 'true') {
         if (text.indexOf(unit) < 0 && parseFloat(text) != 0) {
-          checkFlag.push('第'+(i+1)+'行缺少单位：'+unit+'；')
+          checkFlag.push('第'+(i+1)+'行缺少单位：'+unit+'；\n')
         }
       }
     }
@@ -29,7 +30,9 @@ exports.assertion = function (colDatas, colName, unit) {
   }
   this.command = function (cb) {
     var self = this
-    cb.call(self, colDatas)
+    return this.api.perform(function () {
+      cb.call(self, colDatas)
+    })
     // return this.api.execute(function (selector) {
     //   let dom = document.querySelector(selector)
     //   let text = dom.innerText.trim().replace(/\n/g, '')
--- bin/custom_assertions/fontColor.js
+++ bin/custom_assertions/fontColor.js
@@ -1,24 +1,50 @@
 exports.assertion = function (colDatas, colName) {
-  this.message = '验证【'+colName+'】列的颜色----'
+  this.message = '验证【' + colName + '】列的颜色----'
   this.expected = function () {}
   this.pass = function (data) {
     let checkFlag = [];
     for (let i in data.colors) {
+      i = Number(i)
       let rgb = data.colors[i]
       let text = parseFloat(data.texts[i])
+      let currentColor = false
       if (text > 0) {
-        // this.message += 'red'
+        for (let j in rgb) {
+          let color = rgb[j].split(',')
+          if (color[0] > 128 && color[1] < 128 && color[2] < 128 && Math.abs(color[1] - color[2]) < 100) {
+            currentColor = true
+          }
+        }
+        if (!currentColor) {
+          checkFlag.push('第' + (i + 1) + '行不是红色；\n')
+        }
       } else if (text < 0) {
-        // this.message += 'green'
+        for (let j in rgb) {
+          let color = rgb[j].split(',')
+          if (color[0] < 128 && color[1] > 128 && color[2] < 128 && Math.abs(color[0] - color[2]) < 100) {
+            currentColor = true
+          }
+        }
+        if (!currentColor) {
+          checkFlag.push('第' + (i + 1) + '行不是绿色；\n')
+        }
       } else {
-        // this.message += 'default'
+        for (let j in rgb) {
+          let color = rgb[j].split(',')
+          if (color[0].trim() == color[1].trim() && color[1].trim() == color[2].trim()) {
+            currentColor = true
+          }
+        }
+        if (!currentColor) {
+          checkFlag.push('第' + (i + 1) + '行不是默认色；\n')
+        }
       }
     }
-    if (checkFlag.length === 0){
+    if (checkFlag.length === 0) {
       this.message += '验证通过！'
       return true
     } else {
-      this.message += '验证出错！错误位置：第' + checkFlag.join(',') + '行'
+      this.message += '验证出错！错误位置：' + checkFlag.join(',')
       return false
     }
   }
@@ -27,7 +53,9 @@ exports.assertion = function (colDatas, colName) {
   }
   this.command = function (cb) {
     var self = this
-    cb.call(self, colDatas)
+    return this.api.perform(function () {
+      cb.call(self, colDatas)
+    })
 
     // return this.api.execute(function (selector) {
     //   let dom = document.querySelector(selector)
